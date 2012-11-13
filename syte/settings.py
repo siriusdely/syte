acffd6f6faa0cb29cbae040dd7ac5baeaa181927
Sirius Dely <rious.delie@gmail.com>
2012-11-13 17:48:27 +0700

    try to fix static files not loading on heroku

--- syte/settings.py
+++ syte/settings.py
@@ -8,6 +8,12 @@ import django
 DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT,'static/'),
+)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
