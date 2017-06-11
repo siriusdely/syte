fe7a104e9beeef1937b726ff15bbb901442210da
Sirius Dely <siriusdely@icloud.com>
2017-06-11 08:31:10 +0700

    improve docker containerization

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM python:2.7
 MAINTAINER Sirius Dely
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /code
-RUN mkdir /code/logs
 WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
--- start.sh
+++ start.sh
@@ -5,14 +5,14 @@ python manage.py collectstatic --noinput  # Collect static files
 # Prepare log files and start outputting logs to stdout
 touch ./logs/gunicorn.log
 touch ./logs/access.log
-tail -n 0 -f ./*.log &
+tail -n 0 -f ./logs/*.log &
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn syte.wsgi:application \
     --name sirius_syte \
     --bind 0.0.0.0:8008 \
-    --workers 3 \
+    --workers 1 \
     --log-level=info \
     --log-file=./logs/gunicorn.log \
     --access-logfile=./logs/access.log \
